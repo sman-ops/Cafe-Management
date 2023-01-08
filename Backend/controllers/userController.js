f740cd6a14f3487f02b35a0ab9a03fb9a0296c45
sman-ops <slimen.ghenimi@gmail.com>
2023-01-08 16:05:38 +0100

    implement signin forgotpassword with nodemailer

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,4 +1,8 @@
 const connection = require('../config/connection');
+const jwt = require('jsonwebtoken');
+const nodemailer = require('nodemailer');
+const dotenv = require('dotenv');
+dotenv.config();
 
 exports.signup = (req, res, next) => {
   let user = req.body;
@@ -21,6 +25,82 @@ exports.signup = (req, res, next) => {
         );
       } else {
         return res.status(400).json({ message: 'email already exist' });
+      }
+    } else {
+      return res.status(500).json(err);
+    }
+  });
+};
+
+exports.signin = (req, res, next) => {
+  const user = req.body;
+  query = 'select email,password,role,status from user where email=?';
+  connection.query(query, [user.email], (err, result) => {
+    if (!err) {
+      if (result.length <= 0 || result[0].password != user.password) {
+        return res
+          .status(401)
+          .json({ message: 'incorrect username or password' });
+      } else if (result[0].status === 'false') {
+        return res.status(401).json({ message: 'wait for admin  approvel' });
+      } else if (result[0].password == user.password) {
+        const response = { email: result[0].email, role: result[0].role };
+        const accessToken = jwt.sign(response, process.env.ACCESS_TOKEN, {
+          expiresIn: '9h',
+        });
+        res.status(200).json({ token: accessToken });
+      } else {
+        return res
+          .status(400)
+          .json({ message: 'someting went  wrong please try a later' });
+      }
+    } else {
+      return res.status(500).json(err);
+    }
+  });
+};
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  secure: true,
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD,
+  },
+});
+
+exports.forgotpassword = (req, res, next) => {
+  let user = req.body;
+  query = 'select email,password from user where email=?';
+  connection.query(query, [user.email], (err, result) => {
+    if (!err) {
+      // user not exist in our DB
+      if (result.length <= 0) {
+        return res
+          .status(200)
+          .json({ message: 'Password send succefully to your email' });
+      } else {
+        var mailOptions = {
+          from: process.env.EMAIL,
+          to: result[0].email,
+          subject: 'Password  by cafe management  ',
+          html:
+            '<p><b>Your login details fro cafe managmeent</b><br><b>Email:</b>' +
+            result[0].email +
+            '<br><b>Password:</b>' +
+            result[0].password +
+            '<br><a href="">Click here tol </a></p>',
+        };
+        transporter.sendMail(mailOptions, function (err, info) {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log('Email send' + info.response);
+          }
+        });
+        return res
+          .status(200)
+          .json({ message: 'Password send succefully to your email' });
       }
     } else {
       return res.status(500).json(err);
--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -3,4 +3,6 @@ const userController = require('../controllers/userController');
 const router = express.Router();
 
 router.post('/signup', userController.signup);
+router.post('/signin', userController.signin);
+router.post('/forgotpassword', userController.forgotpassword);
 module.exports = router;
