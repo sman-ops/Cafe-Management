6d5282703051a810c2218de21c1adf69ba7aedbb
sman-ops <slimen.ghenimi@gmail.com>
2023-01-08 12:26:26 +0100

    config folder backend and mysql

---
+++ Backend/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+var cors = require('cors');
+const connection = require('./config/connection');
+const app = express();
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+module.exports = app;
