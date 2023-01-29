e249c8ccef279ecc93d49bcd0e6cb5994d727916
sman-ops <slimen.ghenimi@gmail.com>
2023-01-29 17:56:56 +0100

    complete others api of bill

--- Backend/routes/bill.js
+++ Backend/routes/bill.js
@@ -66,4 +66,72 @@ router.post('/generateReport', auth.authenticateToken, (req, res, next) => {
   );
 });
 
+router.post('/getPdf', auth.authenticateToken, (req, res, next) => {
+  const orderDetails = req.body;
+  const pdfPath = './generated_pdf/' + orderDetails.uuid + 'pdf';
+  if (fs.existsSync(pdfPath)) {
+    res.contentType('application/pdf');
+    fs.createReadStream(pdfPath).pipe(res);
+  } else {
+    var productDetailsReport = JSON.parse(orderDetails.productDetails);
+    ejs.renderFile(
+      path.join(__dirname, '', 'report.ejs'),
+      {
+        productDetails: productDetailsReport,
+        name: orderDetails.name,
+        email: orderDetails.email,
+        contactNumber: orderDetails.contactNumber,
+        paymentMethod: orderDetails.paymentMethod,
+        totalAmount: orderDetails.totalAmount,
+      },
+      (err, results) => {
+        if (err) {
+          return res.status(500).json(err);
+        } else {
+          pdf
+            .create(results)
+            .toFile(
+              './generated_pdf/' + orderDetails.uuid + '.pdf',
+              function (err, data) {
+                if (err) {
+                  console.log(err);
+                  return res.status(500).json(err);
+                } else {
+                  res.contentType('application/pdf');
+                  fs.createReadStream(pdfPath).pipe(res);
+                }
+              }
+            );
+        }
+      }
+    );
+  }
+});
+
+router.get('/getBills', auth.authenticateToken, (req, res, next) => {
+  var query = 'select * from bill order by id DESC';
+  connection.query(query, (err, results) => {
+    if (!err) {
+      return res.status(200).json(results);
+    } else {
+      return res.status(500).json(err);
+    }
+  });
+});
+
+router.delete('/delete/:id', auth.authenticateToken, (req, res, next) => {
+  const { id } = req.params;
+  var query = 'delete from bill where id=?';
+  connection.query(query, [id], (err, results) => {
+    if (!err) {
+      // there is no deletion happen
+      if (results.affectedRows == 0) {
+        return res.status(404).json({ message: 'Bill  id does not found' });
+      }
+      return res.status(200).json({ message: 'Bill deleted successfully' });
+    } else {
+      return res.status(500).json(err);
+    }
+  });
+});
 module.exports = router;
